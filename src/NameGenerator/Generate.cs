9d237b456278567aeac68707a3f088a364e9da2c
Jan Urbaś <janurbas39@gmail.com>
2021-06-17 15:30:07 +0200

    Create class Generate that generates first name, last name and finally full name using both previous methods

---
+++ src/NameGenerator/Generate.cs
@@ -0,0 +1,49 @@
+namespace NameGenerator
+{
+    class Generate 
+    {
+        public static string Name()
+        {
+            var firstName = FirstName();
+            return $"{firstName} {LastName(firstName.Length)}";
+        }
+        private static string FirstName()
+        {
+            var letters = new Alphabet();
+            var fNameLen = Randomize.FirstNameLength();
+            var firstName = Randomize.RandomConsonant().ToUpper();
+
+            while (firstName.Length != fNameLen)
+            {
+                if (letters.vowels.Contains(firstName[firstName.Length - 1]))
+                {
+                    firstName += Randomize.RandomConsonant();
+                }
+                else
+                {
+                    firstName += Randomize.RandomVowel();
+                }
+            }
+            
+            return firstName;
+        }
+        private static string LastName(int fNameLen)
+        {
+                        var letters = new Alphabet();
+            int lNameLen = Randomize.LastNameLength(fNameLen);
+            string lastName = Randomize.RandomConsonant().ToUpper();
+            while (lastName.Length != lNameLen)
+            {
+                if (letters.vowels.Contains(lastName[lastName.Length - 1]))
+                {
+                    lastName += Randomize.RandomConsonant();
+                }
+                else
+                {
+                    lastName += Randomize.RandomVowel();
+                }
+            }
+            return lastName;
+        }
+    }
+}
--- src/NameGenerator/Program.cs
+++ src/NameGenerator/Program.cs
@@ -1,44 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace NameGenerator
+﻿namespace NameGenerator
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var letters = new Alphabet();
-
-            int fNameLen = Randomize.FirstNameLength();
-            int lNameLen = Randomize.LastNameLength(fNameLen);               
-
-            string firstName = Randomize.RandomConsonant().ToUpper();
-            string lastName = Randomize.RandomConsonant().ToUpper();
-
-            while (firstName.Length != fNameLen)
-            {
-                if(letters.vowels.Contains(firstName[firstName.Length-1]))
-                {
-                    firstName += Randomize.RandomConsonant();
-                }
-                else
-                { 
-                    firstName += Randomize.RandomVowel();
-                }
-            }
-
-            while (lastName.Length != lNameLen)
-            {
-                if( letters.vowels.Contains(lastName[lastName.Length-1]))
-                {
-                    lastName += Randomize.RandomConsonant();
-                }
-                else
-                {
-                    lastName += Randomize.RandomVowel();
-                }
-            }
-            System.Console.WriteLine($"{firstName} {lastName}");
+            var name = Generate.Name();
+            System.Console.WriteLine(name);
         }
     }
 }
